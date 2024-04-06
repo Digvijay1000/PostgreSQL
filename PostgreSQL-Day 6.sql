e666494e1118bc77a39778afbb3a9455303f1225
Digvijay Mohite <dlsmohite@gmail.com>
2024-04-06 06:46:43 +0530

    day 6 is work in progress

--- PostgreSQL-Day 6.sql
+++ PostgreSQL-Day 6.sql
@@ -145,8 +145,8 @@ ON b.ticket_no = t.ticket_no
 WHERE 
 t.ticket_no IS NULL; -- just changed col a to col b
 
-/* 3. LEFT OUTER JOIN
-> JOINS THE TWO TABLE WITH ALL DATA FROM LEFT AND MATCHING DATA FROM RIGHT
+/* 3. LEFT OUTER JOIN (LEFT JOIN)
+> JOINS THE TWO TABLE WITH ALL DATA FROM LEFT TABLE AND MATCHING DATA FROM RIGHT TABLE
 
 > SYNTAX: 
         SELECT * FROM TableA
@@ -192,5 +192,72 @@ LEFT JOIN boarding_passes b
 ON s.seat_no = b.seat_no
 GROUP BY RIGHT(s.seat_no,1)
 ORDER BY COUNT(*) DESC;
+
+/* 4. RIGHT OUTER JOIN (RIGHT JOIN)
+> JOINS THE TWO TABLE WITH ALL DATA FROM RIGHT TABLE AND MATCHING DATA FROM LEFT TABLE
+
+> SYNTAX: 
+        SELECT * FROM TableA
+        RIGHT OUTER JOIN TableB
+        ON TableA.employee = TableB.employee
+*/
+-- EXAMPLE
+SELECT * FROM aircrafts_data a
+RIGHT JOIN flights f
+ON a.aircraft_code = f.aircraft_code;
+
+-- find all aircrafts that have not been used in any flight
+SELECT * FROM flights f 
+RIGHT JOIN aircrafts_data a
+ON a.aircraft_code = f.aircraft_code
+WHERE f.flight_id is null;
+
+-- CHALLANGE ON JOINS
+/*
+The company wants to run a phone call campaing on all customers in Texas (=district).
+1. What are the customers (first_name, last_name, phone number and their district) from Texas?
+2. Are there any (old) addresses that are not related to any customer?
+*/
+-- 1. What are the customers (first_name, last_name, phone number and their district) from Texas?
+SELECT first_name, last_name, phone, district FROM customer c
+LEFT JOIN address a
+ON a.address_id = c.address_id
+WHERE district = 'Texas';
+
+-- 2. Are there any (old) addresses that are not related to any customer?
+SELECT * FROM address a
+LEFT JOIN customer c
+ON a.address_id = c.address_id
+WHERE c.customer_id IS null;
+
+SELECT * FROM customer c
+RIGHT JOIN address a
+ON a.address_id = c.address_id
+WHERE c.customer_id IS null;
+
+/* 5. Multiple join conditions (INNER JOIN)
+> JOINS THE TWO TABLE WITH MULTIPLE COLUMNS WITH CONDITION FROM RIGHT TABLE AND MATCHING DATA FROM LEFT TABLE
+
+> SYNTAX: 
+        SELECT * FROM TableA a
+		INNER JOIN TableB b
+		ON a.first_name = b.first_name
+		AND a.last_name = b.last_name
+		
+> Expert Tip :
+        SELECT * FROM TableA a
+		INNER JOIN TableB b
+		ON a.first_name = b.first_name
+		AND a.last_name = 'Jones'           -- More performance efficient than below
+		
+		       or
+			   
+		SELECT * FROM TableA a
+		INNER JOIN TableB b
+		ON a.first_name = b.first_name
+		WHERE a.last_name = 'Jones'
+
+*/
+-- EXAMPLE
 
 
